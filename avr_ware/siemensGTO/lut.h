d8549196c308fdb15996019ecd417bfb8906dbdd
Sitimaru_256 <verderayo0703@outlook.jp>
2026-08-23 20:29:36 +0900

    Update lut.h
    Updated LUT header file to increase array sizes and added SYNC array.

--- avr_ware/siemensGTO/lut.h
+++ avr_ware/siemensGTO/lut.h
@@ -1,19 +1,21 @@
 #ifndef LUT_H
 #define LUT_H
 
-extern const int _7alpha[127][7];
-extern const int _6alpha[127][6];
-extern const int _5alpha[127][5];
-extern const int _4alpha[127][4];
-extern const int _3alpha[127][3];
-extern const int _2alpha[127][2];
-extern const int _1alpha[127][1];
-extern const int _7alpha_pole[127];
-extern const int _6alpha_pole[127];
-extern const int _5alpha_pole[127];
-extern const int _4alpha_pole[127];
-extern const int _3alpha_pole[127];
-extern const int _2alpha_pole[127];
-extern const int _1alpha_pole[127];
+extern const int _7alpha[509][7];
+extern const int _6alpha[509][6];
+extern const int _5alpha[509][5];
+extern const int _4alpha[509][4];
+extern const int _3alpha[509][3];
+extern const int _2alpha[509][2];
+extern const int _1alpha[509][1];
+extern const int _7alpha_pole[509];
+extern const int _6alpha_pole[509];
+extern const int _5alpha_pole[509];
+extern const int _4alpha_pole[509];
+extern const int _3alpha_pole[509];
+extern const int _2alpha_pole[509];
+extern const int _1alpha_pole[509];
+
+extern const int SYNC[5][66];
 
 #endif
